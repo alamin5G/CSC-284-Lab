5a0c3eacb4ac48711c31256a90eefe8c6ab1274c
Md. Alamin <alaminrakib4864@gmail.com>
2022-07-25 19:09:41 +0600

    Modify question 1 and solve it using passing object to Friend Function

--- 5-a2.cpp
+++ 5-a2.cpp
@@ -1,7 +1,7 @@
 /**
  * @file 5-a2.cpp
- * Anupam was a student of BSN program. He continued his study in BSN for 5 semesters. After 5 semesters he thought to change his department from nursing to CSE. 
- * In CSE department he enrolled himself under BCSE Program. While studying BCSE program, he found that he has got weaver for 15 courses. 
+ * Anupam was a student of BSN program. He continued his study in BSN for 5 semesters. After 5 semesters he thought to change his department from nursing to CSE.
+ * In CSE department he enrolled himself under BCSE Program. While studying BCSE program, he found that he has got weaver for 15 courses.
  * That means he completed 45 credit hour in his previous program. Which he need not to do again in BCSE. In BCSE Anupam has completed 18 credit hour.
 1. Now write a program using C++ to find how many credit hours Anupam has completed. NB: Use a member function which will take object as argument or perimeter.
 2. Modify the above scenario from “n” numbers of student. Here user will search for 2 id of different programs and sum their completed credit hours and assigned to the second ID.
@@ -15,42 +15,51 @@
  * I would love be a software engineer at Google. That is why anybody can uses this code without any condition, if you face any difficulty, then try to email me.
  * @version 0.1
  * @date 2022-07-24
- * 
+ *
  * @copyright Copyright (c) 2022
- * 
+ *
  */
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
-class Credits{
+class Credits
+{
+private:
     string name = "Anupam";
     int id;
     int creditHour;
-    public:
-    void setData(){
+
+public:
+    void setData()
+    {
         cout << "Enter Anupam ID: ";
         cin >> id;
         cout << "Enter Anupam credit hours: ";
         cin >> creditHour;
     }
-
-    friend void display(Credits BSN, Credits CSE);
-
-};
-
-void display(Credits BSN, Credits CSE){
-        
-        CSE.creditHour = BSN.creditHour + CSE.creditHour;
+    void display(Credits CSE)
+    {
         cout << CSE.name << " has completed a total of " << CSE.creditHour << " Credit Hours. Whom ID NO. is-:" << CSE.id << endl;
     }
 
-int main(){
+    friend Credits add(Credits BSN, Credits CSE);
+};
+
+Credits add(Credits BSN, Credits CSE)
+{
+    CSE.creditHour = BSN.creditHour + CSE.creditHour;
+    return CSE;
+}
+
+int main()
+{
     Credits BSN;
     cout << "Enter the Information for BSN Department: " << endl;
     BSN.setData();
     Credits CSE;
     cout << "Enter the Information for CSE Department: " << endl;
     CSE.setData();
-    display(BSN, CSE);
-    
+    CSE = add(BSN, CSE);
+    CSE.display(CSE);
+
     return 0;
 }
